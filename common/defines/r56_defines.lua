8b1de8c3107adc1d344edb97353a1185ed37a8a8
PordyHUN <44175470+PordyHUN@users.noreply.github.com>
2019-03-04 18:52:08 +0100

    Added air wind XP levels

--- common/defines/r56_defines.lua
+++ common/defines/r56_defines.lua
@@ -12,9 +12,8 @@ NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 5
 NDefines.NMilitary.UNIT_EXP_LEVELS = {0.02,	0.04, 0.06,	0.08, 0.1, 0.14, 0.18, 0.22, 0.26, 0.3, 0.39, 0.48, 0.57, 0.66, 0.75, 0.78, 0.81, 0.84, 0.87, 0.9}
 NDefines.NMilitary.EXPERIENCE_COMBAT_FACTOR = 0.05
 NDefines.NMilitary.ARMY_EXP_BASE_LEVEL = 5
-
-NDefines.NAir.AIR_WING_XP_LEVELS = {20, 40, 60, 80, 100, 140, 180, 220, 260, 300, 390, 480, 570, 660, 750, 780, 810, 840, 870, 900}, 						--Experience needed to progress to the next level
-NDefines.NAir.AIR_WING_XP_LOSS_WHEN_KILLED = 90,									--if a plane dies, the game assumes that a pilot with this amount of xp died and recalcs average. 
+NDefines.NAir.AIR_WING_XP_LEVELS = {20, 40, 60, 80, 100, 140, 180, 220, 260, 300, 390, 480, 570, 660, 750, 780, 810, 840, 870, 900}
+NDefines.NAir.AIR_WING_XP_LOSS_WHEN_KILLED = 90, 		--if a plane dies, the game assumes that a pilot with this amount of xp died and recalcs average. 
 NDefines.NAir.AIR_WING_XP_TRAINING_MAX = 300.0,
 
 NEW_COMMANDER_RANDOM_PERSONALITY_TRAIT_CHANCES = {  -- chances to gain a personality trait for new generals
