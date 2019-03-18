f1c0921904b14f2888a210665a00c774ca61217c
PordyHUN <44175470+PordyHUN@users.noreply.github.com>
2019-03-18 07:37:47 +0100

    Update r56_defines.lua

--- common/defines/r56_defines.lua
+++ common/defines/r56_defines.lua
@@ -7,7 +7,7 @@ NDefines.NGraphics.COUNTRY_FLAG_LARGE_STRIPE_MAX_HEIGHT = 24000
 NDefines.NMilitary.TRAINING_MAX_LEVEL = 10
 NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 1000			--Max army experience a country can store
 NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 1000			--Max navy experience a country can store
-NDefines.NMilitary.MAX_AIR_EXPERIENCE = 1000				--Max air experience a country can store
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 1150				--Max air experience a country can store
 NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 5
 NDefines.NMilitary.UNIT_EXP_LEVELS = {0.02,	0.04, 0.06,	0.08, 0.1, 0.14, 0.18, 0.22, 0.26, 0.3, 0.39, 0.48, 0.57, 0.66, 0.75, 0.78, 0.81, 0.84, 0.87, 0.9}
 NDefines.NMilitary.EXPERIENCE_COMBAT_FACTOR = 0.05
@@ -22,7 +22,7 @@ NDefines.NAir.AIR_WING_MAX_STATS_SPEED = 1500					-- Used to balance the damage 
 NDefines.NAir.COMBAT_STAT_IMPORTANCE_SPEED = 1.5            -- air superiority scale = superiority/(pixels*this)
 NDefines.NAir.COMBAT_MULTIPLANE_CAP = 4.0
 NDefines.NAir.BASE_STRATEGIC_BOMBING_HIT_PLANE_CHANCE = 1.5		-- Chance to hit a plane in airbase when it is bombed.
-NDefines.NAir.BASE_STRATEGIC_BOMBING_HIT_PLANE_DAMAGE_FACTOR = 1.0
+NDefines.NAir.BASE_STRATEGIC_BOMBING_HIT_PLANE_DAMAGE_FACTOR = 1.5
 NDefines.NAir.COMBAT_BETTER_AGILITY_DAMAGE_REDUCTION = 0.75
 	
 NEW_COMMANDER_RANDOM_PERSONALITY_TRAIT_CHANCES = {  -- chances to gain a personality trait for new generals
