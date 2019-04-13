42868c03efa12dfd0bd677bf24a2e30983b2eb99
Pordányi Dávid <44175470+PordyHUN@users.noreply.github.com>
2019-04-13 09:18:09 +0200

    QoL improvement mostly around air combat

--- common/defines/r56_defines.lua
+++ common/defines/r56_defines.lua
@@ -16,6 +16,7 @@ NDefines.NMilitary.ARMY_EXP_BASE_LEVEL = 5
 
 NDefines.NAir.AIR_WING_XP_LEVELS = {20, 40, 60, 80, 100, 140, 180, 220, 260, 300, 390, 480, 570, 660, 750, 780, 810, 840, 870, 900}
 NDefines.NAir.AIR_WING_XP_TRAINING_MAX = 300.0
+NDefines.NAir.AIR_WING_XP_LOSS_WHEN_KILLED = 200
 NDefines.NAir.AIR_WING_FLIGHT_SPEED_MULT = 0.01					-- Global speed multiplier for airplanes (affects fe.transferring to another base)
 NDefines.NAir.AIR_WING_MAX_STATS_ATTACK = 200					-- Max stats
 NDefines.NAir.AIR_WING_MAX_STATS_DEFENCE = 200
@@ -23,15 +24,16 @@ NDefines.NAir.AIR_WING_MAX_STATS_AGILITY = 200
 NDefines.NAir.AIR_WING_MAX_STATS_SPEED = 1500					-- Used to balance the damage done while bombing.
 NDefines.NAir.COMBAT_STAT_IMPORTANCE_SPEED = 1.5            
 NDefines.NAir.COMBAT_MULTIPLANE_CAP = 4.0
-NDefines.NAir.BASE_STRATEGIC_BOMBING_HIT_PLANE_CHANCE = 1.5		-- Chance to hit a plane in airbase when it is bombed.
-NDefines.NAir.BASE_STRATEGIC_BOMBING_HIT_PLANE_DAMAGE_FACTOR = 1.0
+NDefines.NAir.BASE_STRATEGIC_BOMBING_HIT_PLANE_CHANCE = 2.0		-- Chance to hit a plane in airbase when it is bombed.
+NDefines.NAir.BASE_STRATEGIC_BOMBING_HIT_PLANE_DAMAGE_FACTOR = 2.0
 NDefines.NAir.COMBAT_BETTER_AGILITY_DAMAGE_REDUCTION = 0.75
 NDefines.NAir.DETECT_CHANCE_FROM_NIGHT = -0.8
 NDefines.NAir.BASE_STRATEGIC_BOMBING_HIT_SHIP_CHANCE = 0.1
 NDefines.NAir.BASE_STRATEGIC_BOMBING_HIT_SHIP_DAMAGE_FACTOR = 50
-NDefines.NAir.AIR_COMBAT_FINAL_DAMAGE_SCALE = 0.030
-
 NDefines.NAir.AIR_WING_XP_LOSS_REDUCTION_OVER_FRIENDLY_TERRITORY_FACTOR = 0.7
+NDefines.NAir.AA_INDUSTRY_AIR_DAMAGE_FACTOR = -0.08   --0.12				-- 5x levels = 60% defense from bombing
+NDefines.NAir.AIR_MORE_GROUND_CREWS_COST = 10			--20.0					-- CP cost to maintain more ground crews
+NDefines.NAir.AIR_MORE_GROUND_CREWS_BOOST = 0.2		--0.1					-- Efficienct boost for more ground crews
 
 NEW_COMMANDER_RANDOM_PERSONALITY_TRAIT_CHANCES = {  -- chances to gain a personality trait for new generals
 	0.80, -- 50% for first trait
@@ -63,7 +65,6 @@ NDefines.NNavy.SPOTTING_SPEED_EFFECT_FOR_INITIAL_UNIT_TRANSFER_SPOTTING = 1.20 -
 
 NDefines.NAI.DIVISION_UPGRADE_MIN_XP = 15							                -- Minimum XP before attempting to upgrade a division template.
 NDefines.NAI.DIVISION_CREATE_MIN_XP = 400 --100							            -- Minimum XP before attempting to create a fresh new division template.
-NDefines.NAI.DIVISION_DESIGN_COMBAT_WIDTH_TARGET_WEIGHT = -10000 -- -200	        -- This score is reduced the farther the width is from the target width (if set)
 NDefines.NAI.UPGRADE_PERCENTAGE_OF_FORCES = 0.25 --0.1
 NDefines.NAI.UPGRADES_DEFICIT_LIMIT_DAYS = 365 --180 --50                           -- Ai will avoid upgrading units in the field to new templates if it takes longer than this to fullfill their equipment need
 --------------------------------------------------------------------------------------------------------------
