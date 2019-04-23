4578593b8e6edd243c887275efbf0defa16939fc
Pordányi Dávid <44175470+PordyHUN@users.noreply.github.com>
2019-04-23 16:50:54 +0200

    Added categories to defines rules, modified ITA OOB for further help in AI incompetence.

--- common/defines/r56_defines.lua
+++ common/defines/r56_defines.lua
@@ -4,14 +4,21 @@ NDefines.NGraphics.COUNTRY_FLAG_STRIPE_TEX_MAX_WIDTH = 10
 NDefines.NGraphics.COUNTRY_FLAG_STRIPE_TEX_MAX_HEIGHT = 8196
 NDefines.NGraphics.COUNTRY_FLAG_LARGE_STRIPE_MAX_WIDTH = 41
 NDefines.NGraphics.COUNTRY_FLAG_LARGE_STRIPE_MAX_HEIGHT = 24000
-
+-- Gameplay
 NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 100					-- Days of client lag for decrease of gamespeed
 NDefines.NGame.LAG_DAYS_FOR_PAUSE = 100						-- Days of client lag for pause of gamespeed.
 NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 36
-
-NDefines.NCountry.SPECIAL_FORCES_CAP_BASE = 0.06
-NDefines.NCountry.SPECIAL_FORCES_CAP_MIN = 36
-	
+-- Diplomacy
+NDefines.NGame.BASE_SEND_ATTACHE_COST = 10
+NDefines.NGame.BASE_SEND_ATTACHE_CP_COST = 10.0
+-- Focuses
+NDefines.NFocus.MAX_SAVED_FOCUS_PROGRESS = 20
+-- Country balance
+NDefines.NCountry.SPECIAL_FORCES_CAP_BASE = 0.08
+NDefines.NCountry.SPECIAL_FORCES_CAP_MIN = 40
+NDefines.NCountry.BASE_RESEARCH_SLOTS = 3
+-- Military
+NDefines.NMilitary.ARMY_EXP_BASE_LEVEL = 5
 NDefines.NMilitary.TRAINING_MAX_LEVEL = 10
 NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 1000			--Max army experience a country can store
 NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 1000			--Max navy experience a country can store
@@ -19,10 +26,37 @@ NDefines.NMilitary.MAX_AIR_EXPERIENCE = 1000				--Max air experience a country c
 NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 5
 NDefines.NMilitary.UNIT_EXP_LEVELS = {0.02,	0.04, 0.06,	0.08, 0.1, 0.14, 0.18, 0.22, 0.26, 0.3, 0.39, 0.48, 0.57, 0.66, 0.75, 0.78, 0.81, 0.84, 0.87, 0.9}
 NDefines.NMilitary.EXPERIENCE_COMBAT_FACTOR = 0.05
-NDefines.NMilitary.ARMY_EXP_BASE_LEVEL = 5
 
-NDefines.NTechnology = BASE_RESEARCH_POINTS_SAVED = 50.0
+NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 48
+NDefines.NMilitary.RECON_SKILL_IMPACT = 6
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 8 	--Base cost to unlock a regiment slot,
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 4	--Base cost to change a regiment column.
+NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 6
+NDefines.NMilitary.TACTIC_SWAP_FREQUENCEY = 12
+NDefines.NMilitary.BASE_LEADER_TRAIT_GAIN_XP = 0.7
+NDefines.NMilitary.UNIT_EXPERIENCE_PER_COMBAT_HOUR = 0.000175
+NDefines.NMilitary.FIELD_EXPERIENCE_MAX_PER_DAY = 6
+NDefines.NMilitary.SPEED_REINFORCEMENT_BONUS = 0.1
+NDefines.NMilitary.ENCIRCLED_DISBAND_MANPOWER_FACTOR = 0.0
+NDefines.NMilitary.PLAYER_ORDER_PLANNING_DECAY = 0.005
+NDefines.NMilitary.PLAN_SUPPLY_FACTOR = 0.2
+NDefines.NMilitary.DISBAND_MANPOWER_LOSS = 0.0
+NDefines.NMilitary.BASE_CAPTURE_EQUIPMENT_RATIO = 0.01
+NDefines.NMilitary.FIELD_MARSHAL_XP_RATIO = 0.7
+NDefines.NMilitary.COMMANDER_LEVEL_UP_STAT_WEIGHTS = {5, 5, 3, 4}
+NDefines.NMilitary.NEW_COMMANDER_RANDOM_PERSONALITY_TRAIT_CHANCES = {  -- chances to gain a personality trait for new generals
+	0.80, -- 50% for first trait
+	0.40,  -- 15% for second trait after that
+	0.05  --
+}
+-- Technology
+NDefines.NTechnology.BASE_TECH_COST = 80
+NDefines.NTechnology.BASE_YEAR_AHEAD_PENALTY_FACTOR = 6		-- Base year ahead penalty
+NDefines.NTechnology.BASE_RESEARCH_POINTS_SAVED = 50.0
 
+
+
+-- Air Combat
 NDefines.NAir.AIR_WING_XP_LEVELS = {20, 40, 60, 80, 100, 140, 180, 220, 260, 300, 390, 480, 570, 660, 750, 780, 810, 840, 870, 900}
 NDefines.NAir.AIR_WING_XP_TRAINING_MAX = 300.0
 NDefines.NAir.AIR_WING_XP_LOSS_WHEN_KILLED = 200
@@ -35,58 +69,17 @@ NDefines.NAir.COMBAT_STAT_IMPORTANCE_SPEED = 1.5
 NDefines.NAir.COMBAT_MULTIPLANE_CAP = 2.0
 NDefines.NAir.DETECT_CHANCE_FROM_OCCUPATION = 0.15
 NDefines.NAir.ACCIDENT_CHANCE_BASE = 0.03
+NDefines.NAir.COMBAT_DAMAGE_SCALE = 0.2
+NDefines.NAir.EFFICIENCY_REGION_CHANGE_DAILY_GAIN_TACTICAL_BOMBER = 0.100
+NDefines.NAir.EFFICIENCY_REGION_CHANGE_DAILY_GAIN_STRATEGIC_BOMBER = 0.036
 NDefines.NAir.AIR_WING_COUNTRY_XP_FROM_TRAINING_FACTOR = 0.0075
+NDefines.NAir.DISRUPTION_ATTACK_FACTOR = 1.0
 NDefines.NAir.BASE_STRATEGIC_BOMBING_HIT_PLANE_CHANCE = 2.0		-- Chance to hit a plane in airbase when it is bombed.
 NDefines.NAir.BASE_STRATEGIC_BOMBING_HIT_PLANE_DAMAGE_FACTOR = 2.0
 NDefines.NAir.DETECT_CHANCE_FROM_NIGHT = -0.9
 NDefines.NAir.BASE_STRATEGIC_BOMBING_HIT_SHIP_CHANCE = 0.1
 NDefines.NAir.BASE_STRATEGIC_BOMBING_HIT_SHIP_DAMAGE_FACTOR = 50
 NDefines.NAir.AIR_WING_XP_LOSS_REDUCTION_OVER_FRIENDLY_TERRITORY_FACTOR = 0.7
-NDefines.NCountry.BASE_RESEARCH_SLOTS = 3
-NDefines.NTechnology.BASE_TECH_COST = 80
-NDefines.NTechnology.BASE_YEAR_AHEAD_PENALTY_FACTOR = 6		-- Base year ahead penalty
-
-NDefines.NPolitics.DEFAULT_OCCUPATION_POLICY = 0
-NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 48
-NDefines.NMilitary.RECON_SKILL_IMPACT = 6
-NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 8 	--Base cost to unlock a regiment slot,
-NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 4	--Base cost to change a regiment column.
-NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 6
-NDefines.NMilitary.LAND_COMBAT_STR_DAMAGE_MODIFIER = 0.08
-NDefines.NMilitary.TACTIC_SWAP_FREQUENCEY = 12
-NDefines.NMilitary.BASE_LEADER_TRAIT_GAIN_XP = 0.7
-NDefines.NMilitary.UNIT_EXPERIENCE_PER_COMBAT_HOUR = 0.000175
-NDefines.NMilitary.FIELD_EXPERIENCE_MAX_PER_DAY = 6
-NDefines.NMilitary.SPEED_REINFORCEMENT_BONUS = 0.1
-NDefines.NMilitary.ENCIRCLED_DISBAND_MANPOWER_FACTOR = 0.0
-NDefines.NMilitary.PLAYER_ORDER_PLANNING_DECAY = 0.01
-NDefines.NMilitary.PLAN_SUPPLY_FACTOR = 0.2
-NDefines.NMilitary.DISBAND_MANPOWER_LOSS = 0.0
-NDefines.NMilitary.BASE_CAPTURE_EQUIPMENT_RATIO = 0.01
-NDefines.NMilitary.FIELD_MARSHAL_XP_RATIO = 0.7
-NDefines.NMilitary.COMMANDER_LEVEL_UP_STAT_WEIGHTS = {5, 5, 3, 4}
-NDefines.NAI.ESCORT_BOMBERS_IMPORTANCE_MODIFIER
-NDefines.NAI.FALLBACK_LOSING_FACTOR = 0
-NDefines.NAI.HOUR_BAD_COMBAT_REEVALUATE = 24
-NDefines.NAI.MAX_FUEL_CONSUMPTION_RATIO_FOR_NAVY_TRAINING = 1 				-- ai will use at most this ratio of affordable fuel for naval training
-NDefines.NAI.MAX_FUEL_CONSUMPTION_RATIO_FOR_AIR_TRAINING = 1
-NDefines.NAI.ORG_UNIT_WEAK = 0.45
-NDefines.NAI.STR_UNIT_WEAK = 0.4
-NDefines.NAI.ORG_UNIT_NORMAL = 0.6
-NDefines.NAI.STR_UNIT_NORMAL = 0.5
-NDefines.NAI.VARIANT_UPGRADE_MIN_XP = 200
-
-NDefines.NFocus.MAX_SAVED_FOCUS_PROGRESS = 20
-NEW_COMMANDER_RANDOM_PERSONALITY_TRAIT_CHANCES = {  -- chances to gain a personality trait for new generals
-	0.80, -- 50% for first trait
-	0.40,  -- 15% for second trait after that
-	0.05  --
-}
-
-NCountry = {
-	BASE_MOBILIZATION_SPEED = 0.015, -- from 0.01 to 0.015
-}
-
 NDefines.NAir.MISSION_COMMAND_POWER_COSTS = {  -- command power cost per plane to create a mission
 		0.0, -- AIR_SUPERIORITY
 		0.0, -- CAS		
@@ -102,11 +95,26 @@ NDefines.NAir.MISSION_COMMAND_POWER_COSTS = {  -- command power cost per plane t
 		0.0, -- NAVAL_MINES_PLANTING
 		0.0, -- NAVAL_MINES_SWEEPING		  							  						  
 	}
+-- Politics
+NDefines.NPolitics.DEFAULT_OCCUPATION_POLICY = 0
+-- Ai behaviour
+NDefines.NAI.FALLBACK_LOSING_FACTOR = 0
+NDefines.NAI.HOUR_BAD_COMBAT_REEVALUATE = 24
+NDefines.NAI.MAX_FUEL_CONSUMPTION_RATIO_FOR_NAVY_TRAINING = 1 				-- ai will use at most this ratio of affordable fuel for naval training
+NDefines.NAI.MAX_FUEL_CONSUMPTION_RATIO_FOR_AIR_TRAINING = 1
+NDefines.NAI.ORG_UNIT_WEAK = 0.45
+NDefines.NAI.STR_UNIT_WEAK = 0.4
+NDefines.NAI.ORG_UNIT_NORMAL = 0.6
+NDefines.NAI.STR_UNIT_NORMAL = 0.5
+NDefines.NAI.VARIANT_UPGRADE_MIN_XP = 200
+NDefines.NAI.GARRISON_FRACTION = 0.05
 
--- QoL improvement:
---------------------------------------------------------------------------------------------------------------
--- GAMEPLAY
---------------------------------------------------------------------------------------------------------------
+-- Navy
+NDefines.NNavy.NAVAL_MINES_IN_REGION_MAX = 5000.0
+NDefines.NNavy.NAVAL_MINES_SWEEPING_SPEED_MULT = 0.01
+NDefines.NNavy.NAVAL_MINES_NAVAL_SUPREMACY_FACTOR = 0.75
+NDefines.NNavy.NAVAL_MINES_ACCIDENT_CRITICAL_HIT_CHANCES = 0.10
+NDefines.NNavy.NAVAL_MINES_ACCIDENT_STRENGTH_LOSS = 50.0
 NDefines.NNavy.UNIT_TRANSFER_SPOTTING_SPEED_MULT = 1.0 -- 5.0						-- spotting speed mult against unit transfers
 NDefines.NNavy.UNIT_TRANSFER_DETECTION_CHANCE_BASE = 4.17 --25.02						-- unit transfer and naval invasion base chance detection percentage (if this fails no detection is done on that tick)
 NDefines.NNavy.BASE_SPOTTING_EFFECT_FOR_INITIAL_UNIT_TRANSFER_SPOTTING = 0.35 --10.0	-- same as BASE_SPOTTING_EFFECT_FOR_INITIAL_CONVOY_SPOTTING but for naval transfer convoys
