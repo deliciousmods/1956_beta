2922ebc1f1a4e3cfedac04a7a09708448a8a1cad
featherus <featherbook1@gmail.com>
2020-12-31 10:57:35 +0000

    State Changes (#1541)
    * Make Taklamakan impassable
    
    * Make VPs more visible
    
    * Add Korean States
    
    * KOR Focus tree improvements
    
    * Add Mongolian States
    
    * Missed Mongolain state check
    
    * And another Mongolian state check
    
    * Add Thai Claimed States to Laos and Cambodia
    
    * Rework Vietnam states
    
    * TNG Fixes

--- common/defines/r56_defines.lua
+++ common/defines/r56_defines.lua
@@ -5,6 +5,9 @@ NDefines.NGraphics.COUNTRY_FLAG_STRIPE_TEX_MAX_WIDTH = 10
 NDefines.NGraphics.COUNTRY_FLAG_STRIPE_TEX_MAX_HEIGHT = 8196
 NDefines.NGraphics.COUNTRY_FLAG_LARGE_STRIPE_MAX_WIDTH = 41
 NDefines.NGraphics.COUNTRY_FLAG_LARGE_STRIPE_MAX_HEIGHT = 24000
+NDefines.NGraphics.VICTORY_POINT_MAP_ICON_AFTER = {0, 20} -- After this amount of VP the map icon becomes bigger dot.
+NDefines.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {200, 500, 750} -- At what camera distance the VP name text disappears.
+NDefines.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {300, 750, 1000} -- At what distance VPs are hidden
 -- Focuses
 NDefines.NFocus.MAX_SAVED_FOCUS_PROGRESS = 14
 
