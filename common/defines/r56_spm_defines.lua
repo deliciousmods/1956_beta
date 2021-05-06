b174e7b763b96bc5a1b9f4eeadd98fd42a8a6ccd
Shizophren <dynamiter@outlook.de>
2021-05-07 00:15:10 +0200

    decisions

--- common/defines/r56_spm_defines.lua
+++
@@ -1,4 +0,0 @@
--- Gameplay
-NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 75
-NDefines.NGame.LAG_DAYS_FOR_PAUSE = 75
-NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 24
--- descriptor.mod
+++
@@ -1,6 +0,0 @@
-version="1.10.2"
-tags={
-	"Beta"
-}
-name="The Road to 56 [Beta]"
-remote_file_id="1088963694"
